8c1ff6f34ebb6cee485393f1b2baf021bb00d2b6
sreerag.girijan <sreerag.girijan@aspiresys.com>
2024-12-24 18:08:38 +0530

    Add basic structure and functionality for "Guess the Number" game

---
+++ 24_12_2024/guess.js
@@ -0,0 +1,48 @@
+document.querySelector(".guess-input").style.boxShadow = "3px 3px 6px black";
+guessCount=1;
+
+  function checkGuess() {
+    // const userGuess = Number(document.getElementById('guess-input').value);
+    const userGuess = Number(document.querySelector('.guess-input').value);
+
+    console.log(userGuess);
+    
+    if (guessCount===1){
+        randomNumber = Math.floor(Math.random() * 100) + 1;
+        // randomNumber=5;
+    }
+    
+    if (userGuess===randomNumber){
+        para=document.querySelector(".output");
+        para.textContent="Congratulations! You got it right! random number reseted";
+        para.style.backgroundColor="green";
+        guessCount=0;
+    }
+    else if (userGuess<randomNumber){
+        para=document.querySelector(".output");
+        para.textContent="Wrong! Your guess is too low!";
+        para.style.backgroundColor="blue";
+    }else{
+        para=document.querySelector(".output");
+        para.textContent="Wrong! Your guess is too high!";
+        para.style.backgroundColor="red";
+    }
+    guessCount++;
+
+  }
+
+  
+  const button = document.querySelector("button");
+  button.addEventListener("click", checkGuess);
+
+
+const buttonA = document.querySelector("#button_A");
+const headingA = document.querySelector("#heading_A");
+
+let count = 1;
+
+buttonA.onclick = () => {
+  buttonA.textContent = "Try again!";
+  headingA.textContent = `${count} clicks so far`;
+  count += 1;
+};
