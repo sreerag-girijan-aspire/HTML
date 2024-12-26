44767fad3255f0ea1826adab15233288dccecf6f
sreerag.girijan <sreerag.girijan@aspiresys.com>
2024-12-26 16:18:13 +0530

    Add array manipulation examples and console logs for fruits

--- 26_12_2024/index.js
+++ 26_12_2024/index.js
@@ -41,3 +41,26 @@ const updated = browserType.replace("moz", "van");
 
 console.log(updated); // "vanilla"
 console.log(browserType); // "mozilla"
+
+
+const fruits = [];
+fruits.push("banana", "apple", "peach");
+console.log(fruits.length); // 3
+
+fruits[5] = "mango";
+console.log(fruits[5]); // 'mango'
+console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+console.log(fruits.length); // 6
+console.log(fruits);
+
+fruits.length = 10;
+console.log(fruits); // ['banana', 'apple', 'peach', empty x 2, 'mango', empty x 4]
+console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+console.log(fruits.length); // 10
+console.log(fruits[8]); // undefined
+
+
+fruits.length = 2;
+console.log(Object.keys(fruits)); // ['0', '1']
+console.log(fruits.length); // 2
+console.log(fruits);
