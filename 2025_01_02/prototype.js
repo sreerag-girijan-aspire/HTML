fb584c2a9fb31dffec8150f54e55d1fb20150a6f
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-02 17:58:22 +0530

    Add properties HTML and JavaScript files for date object demonstration and prototype exploration

--- 2025_01_02/prototype.js
+++ 2025_01_02/prototype.js
@@ -12,3 +12,17 @@ const myCity = {
   console.log(typeof(myCity));
   console.log(myCity.__proto__);
   console.log(Object.getPrototypeOf(myCity));
+
+
+
+const myDate = new Date();
+let object = myDate;
+
+do {
+  object = Object.getPrototypeOf(object);
+  console.log(object);
+} while (object);
+
+// Date.prototype
+// Object { }
+// null
