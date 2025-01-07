618086c1e7d35b5fc05205b0f426b5d6bcda26e4
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-07 17:22:48 +0530

    Add HTML and JavaScript files to demonstrate template strings

---
+++ 2025_01_07/templateString.js
@@ -0,0 +1,19 @@
+console.log("string text line 1\n" + "string text line 2");
+// "string text line 1
+// string text line 2"
+
+
+
+console.log(`string text line 1
+        string text line 2`);
+    // "string text line 1
+    // string text line 2"
+    
+
+
+const a = 5;
+const b = 10;
+console.log(`Fifteen is ${a + b} and
+not ${2 * a + b}.`);
+// "Fifteen is 15 and
+// not 20."
