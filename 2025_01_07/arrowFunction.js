cbef546d3eb9de96ee3eb0ae3ad8e8cee1c264b9
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-07 17:51:09 +0530

    Add HTML and JavaScript files to demonstrate arrow functions

---
+++ 2025_01_07/arrowFunction.js
@@ -0,0 +1,19 @@
+// () => expression
+
+// param => expression
+
+// (param) => expression
+
+// (param1, paramN) => expression
+
+// () => {
+// statements
+// }
+
+// param => {
+// statements
+// }
+
+// (param1, paramN) => {
+// statements
+// }
