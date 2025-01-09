5204d58c1619eedbd9813a804710aa4afc60e6c8
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-09 11:57:57 +0530

    Add JavaScript examples for destructuring assignment

--- 2025_01_07/destructuring.js
+++ 2025_01_07/destructuring.js
@@ -1,3 +1,27 @@
-const result = /(a+)(b+)(c+)/.exec("aaabcc");
-const [, a, b, c] = result;
+const result = /(a+)(b+)(c+)(d+)(e+)(f+)/.exec("aaabccdddeefff");
+[, a, b, c] = result;
 console.log(a, b, c); // "aaa" "b" "cc"
+console.log(result);
+
+[, a, b, c,...d] = result;
+console.log(a, b, c, d);
+
+
+const obj = { a: 1, b: 2 };
+var { a, b } = obj;
+console.log(a, b);
+
+
+var { a:a1, b:b1 } = obj;
+console.log(a1, b1);
+
+let a = 1;                  //
+let b = 3;                  //
+[a, b] = [b, a];            // swap
+console.log(a); // 3        //    
+console.log(b); // 1        //
+
+const arr = [1, 2, 3];
+[arr[2], arr[1]] = [arr[1], arr[2]];
+console.log(arr); // [1, 3, 2]
+
