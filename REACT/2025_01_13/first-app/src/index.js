fd2e328548f57ab4ae90ed27c531a2722ec31cd0
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-14 18:06:44 +0530

    Add List component to display a list of products

--- REACT/2025_01_13/first-app/src/index.js
+++ REACT/2025_01_13/first-app/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AboutPage from './About';
 import Profile from './Profile';
+import List from './List';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     <App />
     <AboutPage/>
     <Profile/>
+    <List/>
   </React.StrictMode>
 );
 
