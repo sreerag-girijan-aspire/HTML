34d992ec9301b425506d81d1c5ec380e8c4df75f
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-14 17:36:45 +0530

    Add About page styling with CSS for header

--- REACT/2025_01_13/first-app/src/About.js
+++ REACT/2025_01_13/first-app/src/About.js
@@ -1,7 +1,8 @@
+import "./About.css";
 function AboutPage() {
     return (
       <>
-        <h1>About</h1>
+        <h1 className="h1">About</h1>
         <p>Hello there.<br />How do you do?</p>
       </>
     );
