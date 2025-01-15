27838b7fbeba6db8d234e383cd571f4dc39b448a
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-15 22:43:47 +0530

    Update List component to include isFruit property and style list items accordingly

--- REACT/2025_01_13/first-app/src/List.js
+++ REACT/2025_01_13/first-app/src/List.js
@@ -1,13 +1,16 @@
 
 
 const products = [
-    { title: 'Cabbage', id: 1 },
-    { title: 'Garlic', id: 2 },
-    { title: 'Apple', id: 3 },
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
   ];
 export default function List(){
   const listItems = products.map(product =>
-    <li key={product.id}>
+    <li key={product.id}
+    style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}>
       {product.title}
     </li>
   );
