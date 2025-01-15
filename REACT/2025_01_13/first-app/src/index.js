a87ddab924edaf34001670404d7475746d8341f5
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-15 20:36:50 +0530

    Add MyApp component with separate counters using useState

--- REACT/2025_01_13/first-app/src/index.js
+++ REACT/2025_01_13/first-app/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import AboutPage from './About';
 import Profile from './Profile';
 import List from './List';
+import MyAppUseState from './UseState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +15,7 @@ root.render(
     <AboutPage/>
     <Profile/>
     <List/>
+    <MyAppUseState/>
   </React.StrictMode>
 );
 
